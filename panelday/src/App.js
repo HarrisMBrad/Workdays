114cd248caa9e40c0b85ba9d08d10ffc38a7b441
eryday-keif <55902661+HarrisMBrad-cell@users.noreply.github.com>
2023-02-21 06:10:55 -0800

    removed old for to merge the needed.

--- panelday/src/App.js
+++ panelday/src/App.js
@@ -1,31 +1,3 @@
-import React, { useState } from 'react';
-import './App.css';
-
-let canvas = document.getElementById("myWelcomeCanvas");
-let ctx = canvas.getContext("like-button-root");
-ctx("myWelcome");
-// This feature has the user make three clicks in a row to see welcome. 
-export default function App() {
-
-  const [count, setCount] = useState(0);
-
-
-  return (
-
-    <body className='container'>
-      <p>You liked this {count} times</p>
-      <button onUploadImage={() => alert('like-button-root')} />
-      <>
-        <section id="like-button-root">  {(<button onClick={setCount(count + 1 + "myWelcome")}>Welcome</button>)}
-         
-        </section>
-      </>
-
-      <script src="https://unpkg.com/react@18/umd/react.development.js" crossOrigin></script>
-      <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossOrigin></script>
-      <script src="../off/likeBtn.js"></script>
-    </body>
-
-  );
-}
-
+// import React, { useState } from 'react';
+// import './App.css';
+// was hoping to pull the dev-branch for this file...
